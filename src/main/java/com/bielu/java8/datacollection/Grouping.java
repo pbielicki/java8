40c995daf4dd6bb5b65d9da639827bf5e8139f8f
pbielicki <przemyslaw.bielicki@amadeus.com>
2015-01-18 14:11:37 +0100

    Added more examples for grouping

--- src/main/java/com/bielu/java8/datacollection/Grouping.java
+++ src/main/java/com/bielu/java8/datacollection/Grouping.java
@@ -1,7 +1,10 @@
 package com.bielu.java8.datacollection;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import com.bielu.java8.domain.Dish;
@@ -30,6 +33,42 @@ public class Grouping {
             })));
     
     System.out.println(dishesByTypeCaloricLevel);
+    
+    // subgroups
+    Map<DishType, Long> typesCount = DomainFactory.menu()
+        .stream()
+        .collect(Collectors.groupingBy(Dish::getType, Collectors.counting()));
+    
+    Map<DishType, Optional<Dish>> mostCaloricByTypeOptional =
+        DomainFactory.menu()
+        .stream()
+        .collect(Collectors.groupingBy(
+            Dish::getType, Collectors.maxBy(
+                Comparator.comparing(Dish::getCalories))));
+
+    Map<DishType, Dish> mostCaloricByType =
+        DomainFactory.menu()
+        .stream()
+        .collect(Collectors.groupingBy(Dish::getType, 
+            Collectors.collectingAndThen(
+                Collectors.maxBy(Comparator.comparing(Dish::getCalories)),
+                Optional::get)));
+    
+    System.out.println(typesCount);
+    System.out.println(mostCaloricByTypeOptional);
+    System.out.println(mostCaloricByType);
+    
+    Map<DishType, Set<CaloricLevel>> caloricLevelsByType =
+        DomainFactory.menu()
+        .stream()
+        .collect(Collectors.groupingBy(Dish::getType,
+            Collectors.mapping(dish -> {
+              if (dish.getCalories() <= 400) return CaloricLevel.DIET;
+              if (dish.getCalories() <= 700) return CaloricLevel.NORMAL;
+              return CaloricLevel.FAT;
+            }, Collectors.toSet())));
+    
+    System.out.println(caloricLevelsByType);
   }
 
 }
