8eeff2c96ae53c573d9e785c7a94db5e06f8ad78
pbielicki <przemyslaw.bielicki@amadeus.com>
2015-01-22 21:32:58 +0100

    Added complex and shitworthy Collector example collecting prime numbers

---
+++ src/main/java/com/bielu/java8/collector/PrimeNumberCollector.java
@@ -0,0 +1,81 @@
+package com.bielu.java8.collector;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.IntStream;
+
+public class PrimeNumberCollector implements Collector<Integer, Map<Boolean, List<Integer>>, Map<Boolean, List<Integer>>> {
+
+  public static void main(String[] args) {
+    System.out.println(IntStream.rangeClosed(2, 100)
+        .boxed()
+        .collect(new PrimeNumberCollector()));
+  }
+  
+  @SuppressWarnings("serial")
+  @Override
+  public Supplier<Map<Boolean, List<Integer>>> supplier() {
+    return () -> new HashMap<Boolean, List<Integer>>() {
+      {
+        put(true, new ArrayList<>());
+        put(false, new ArrayList<>());
+      }
+    };
+  }
+
+  @Override
+  public BiConsumer<Map<Boolean, List<Integer>>, Integer> accumulator() {
+    return (acc, candidate) -> {
+      acc.get(isPrime(acc.get(true), candidate)).add(candidate);
+    };
+  }
+
+  private boolean isPrime(List<Integer> primes, Integer candidate) {
+    int candidateRoot = (int) Math.sqrt((double) candidate);
+    return takeWhile(primes, i -> i <= candidateRoot)
+        .stream()
+        .noneMatch(p -> candidate % p == 0);
+  }
+
+  private <A> List<A> takeWhile(List<A> list, Predicate<A> p) {
+    int i = 0;
+    for (A item : list) {
+      if (!p.test(item)) {
+        return list.subList(0,  i);
+      }
+      i++;
+    }
+    return list;
+  }
+
+  @Override
+  public BinaryOperator<Map<Boolean, List<Integer>>> combiner() {
+    return (map1, map2) -> {
+      map1.get(true).addAll(map2.get(true));
+      map1.get(false).addAll(map2.get(false));
+      return map1;
+    };
+  }
+
+  @Override
+  public Function<Map<Boolean, List<Integer>>, Map<Boolean, List<Integer>>> finisher() {
+    return Function.identity();
+  }
+
+  @Override
+  public Set<Characteristics> characteristics() {
+    return Collections.unmodifiableSet(EnumSet.of(Characteristics.IDENTITY_FINISH));
+  }
+
+}
